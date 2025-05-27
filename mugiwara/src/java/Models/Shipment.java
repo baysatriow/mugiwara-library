fdc1d635af0ed6191fce31cd576568abb2fbae96
gilbert19-commits <gilbertlt19@gmail.com>
2025-05-27 10:44:39 +0700

    feat: Update Models

---
+++ mugiwara/src/java/Models/Shipment.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author VIVOBOOK
+ */
+public class Shipment {
+    private Order order;
+    private String trackingNumber;
+    private String status;
+    private ShippingMethod shippingMethod;
+    private Date estimated;
+    
+}
