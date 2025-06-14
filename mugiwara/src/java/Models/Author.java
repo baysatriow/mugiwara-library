f33a20d62e404ed45be05ab5e1a7abbe604d9274
dicodella.go <dicodella.go@gmail.com>
2025-06-14 15:39:44 +0700

    update Author

--- mugiwara/src/java/Models/Author.java
+++ mugiwara/src/java/Models/Author.java
@@ -3,33 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Models;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author ASUS
  */
 public class Author {
+    private int authorId;
     private String name;
     private String description;
-    private Book[] books;
+    private List<Book> books = new ArrayList<>();
     
     public Author(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    public Author(int id, String name, String description) {
+        this.authorId = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getAuthorId(){
+        return authorId;
+    }
+
     public String getName() {
         return name;
     }
 
-    public void setBooks(Book[] books) {
+    public String getDescription(){
+        return description;
+    }
+
+    public List<Book> getBooks(){
+        return books;
+    }
+
+    public void setId(int id){
+        this.authorId = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setBooks(List<Book> books) {
         this.books = books;
     }
     
     public void authorInfo(){
         System.out.println("Penulis: " + name);
         System.out.println("Deskripsi: " + description);
-        if (books != null) {
+        if (books != null && !books.isEmpty())  {
             System.out.println("Books:");
             for (Book book : books) {
                 System.out.println(" - " + book.getTitle()); 
